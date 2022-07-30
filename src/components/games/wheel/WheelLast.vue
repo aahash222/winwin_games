45a02682a869457ea20984e73d58f32aadc07484
nofik44 <nofilet@protonmail.com>
2022-07-31 00:55:01 +0300

    wheel end

--- src/components/games/wheel/WheelLast.vue
+++ src/components/games/wheel/WheelLast.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>last</div>
+  <div class="bet-last">
+    <transition-group name="list-last-bets" class="bet-last-bets" tag="div">
+      <div v-for="item in bets" :key="item.uuid" class="bet-last-best-item" :class="{ lose: item.payout === 0 }" @click="openBet(item.uuid)">
+        <!--<div class="bet-last-best-item-payout">x{{ item.payout }}</div>-->
+        <!--<div>{{ item.result }}</div>-->
+        <div>x{{ item.payout }}</div>
+      </div>
+    </transition-group>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'WheelLast'
+  name: 'WheelLast',
+  props: {
+    bets: { type: Array }
+  },
+  methods: {
+    openBet: function(uuid) {
+      this.$store.commit('openModalBet', uuid)
+    }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style lang="scss" src="@/assets/styles/bet-last.scss" />
