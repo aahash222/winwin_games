470be0e95d7c16e39c87becafc4d2741cd3d2465
nofik44 <nofilet@protonmail.com>
2022-07-31 00:54:36 +0300

    wheel end

--- src/store/socket.js
+++ src/store/socket.js
@@ -1,5 +1,4 @@
 import main from '../main'
-import { context } from 'vue-native-websocket-vue3/webstorm.config'
 
 const moduleSocket = {
   state: {
@@ -35,10 +34,19 @@ const moduleSocket = {
         mines_select: null,
         mines_cashout: null,
 
+        plinko: null,
+        plinko_bet: null,
+
         tower: null,
         tower_bet: null,
         tower_select: null,
         tower_cashout: null,
+
+        wheel: null,
+        wheel_bet: null,
+
+        roulette: null,
+        roulette_bet: null,
       }
     }
   },
@@ -58,10 +66,19 @@ const moduleSocket = {
     subscribeSocketMinesSelect: state => state.socket.subscribes.mines_select,
     subscribeSocketMinesCashout: state => state.socket.subscribes.mines_cashout,
 
+    subscribeSocketPlinko: state => state.socket.subscribes.plinko,
+    subscribeSocketPlinkoBet: state => state.socket.subscribes.plinko_bet,
+
     subscribeSocketTower: state => state.socket.subscribes.tower,
     subscribeSocketTowerBet: state => state.socket.subscribes.tower_bet,
     subscribeSocketTowerSelect: state => state.socket.subscribes.tower_select,
     subscribeSocketTowerCashout: state => state.socket.subscribes.tower_cashout,
+
+    subscribeSocketWheel: state => state.socket.subscribes.wheel,
+    subscribeSocketWheelBet: state => state.socket.subscribes.wheel_bet,
+
+    subscribeSocketRoulette: state => state.socket.subscribes.roulette,
+    subscribeSocketRouletteBet: state => state.socket.subscribes.roulette_bet,
   },
   actions: {
     socketConnect: (context) => {
@@ -85,9 +102,15 @@ const moduleSocket = {
     sendSocketMinesSelect: (context, data) => { context.commit('socketSend', { action: 'mines_select', data: data }) },
     sendSocketMinesCashout: (context, data) => { context.commit('socketSend', { action: 'mines_cashout', data: data }) },
 
+    sendSocketPlinkoBet: (context, data) => { context.commit('socketSend', { action: 'plinko_bet', data: data }) },
+
     sendSocketTowerBet: (context, data) => { context.commit('socketSend', { action: 'tower_bet', data: data }) },
     sendSocketTowerSelect: (context, data) => { context.commit('socketSend', { action: 'tower_select', data: data }) },
     sendSocketTowerCashout: (context, data) => { context.commit('socketSend', { action: 'tower_cashout', data: data }) },
+
+    sendSocketWheelBet: (context, data) => { context.commit('socketSend', { action: 'wheel_bet', data: data }) },
+
+    sendSocketRouletteBet: (context, data) => { context.commit('socketSend', { action: 'roulette_bet', data: data }) },
 
     sendSocketPong: (context) => {
       context.commit('socketSend', { action: 'pong', data: null })
@@ -103,7 +126,7 @@ const moduleSocket = {
 
   mutations: {
     socketSend: (state, { action, data }) => {
-      console.log('send to socket ', action)
+      if (action !== 'pong') console.log('send to socket ', action)
       main.config.globalProperties.$socket.sendObj({ action: action, data: data })
     },
     socketSetAddress: (state, game) => {
@@ -122,6 +145,12 @@ const moduleSocket = {
           break
         case 'Wheel':
           state.socket.address = process.env.VUE_APP_WS_WHEEL
+          break
+        case 'Roulette':
+          state.socket.address = process.env.VUE_APP_WS_ROULETTE
+          break
+        case 'Hilo':
+          state.socket.address = process.env.VUE_APP_WS_HILO
           break
       }
       console.log('set socket address: ', game, state.socket.address)
@@ -152,10 +181,19 @@ const moduleSocket = {
     subscribeSocketMinesSelect: state => { state.socket.subscribes.mines_select = null },
     subscribeSocketMinesCashout: state => { state.socket.subscribes.mines_cashout = null },
 
+    subscribeSocketPlinko: state => { state.socket.subscribes.plinko = null },
+    subscribeSocketPlinkoBet: state => { state.socket.subscribes.plinko_bet = null },
+
     subscribeSocketTower: state => { state.socket.subscribes.tower = null },
     subscribeSocketTowerBet: state => { state.socket.subscribes.tower_bet = null },
     subscribeSocketTowerSelect: state => { state.socket.subscribes.tower_select = null },
     subscribeSocketTowerCashout: state => { state.socket.subscribes.tower_cashout = null },
+
+    subscribeSocketWheel: state => { state.socket.subscribes.wheel = null },
+    subscribeSocketWheelBet: state => { state.socket.subscribes.wheel_bet = null },
+
+    subscribeSocketRoulette: state => { state.socket.subscribes.roulette = null },
+    subscribeSocketRouletteBet: state => { state.socket.subscribes.roulette_bet = null },
 
     SOCKET_session (state, data) { state.socket.subscribes.session = data },
     SOCKET_server_seed_next (state, data) { state.socket.subscribes.server_seed_next = data },
@@ -168,10 +206,19 @@ const moduleSocket = {
     SOCKET_mines_select (state, data) { state.socket.subscribes.mines_select = data },
     SOCKET_mines_cashout (state, data) { state.socket.subscribes.mines_cashout = data },
 
+    SOCKET_plinko (state, data) { state.socket.subscribes.plinko = data },
+    SOCKET_plinko_bet (state, data) { state.socket.subscribes.plinko_bet = data },
+
     SOCKET_tower (state, data) { state.socket.subscribes.tower = data },
     SOCKET_tower_bet (state, data) { state.socket.subscribes.tower_bet = data },
     SOCKET_tower_select (state, data) { state.socket.subscribes.tower_select = data },
     SOCKET_tower_cashout (state, data) { state.socket.subscribes.tower_cashout = data },
+
+    SOCKET_wheel (state, data) { state.socket.subscribes.wheel = data },
+    SOCKET_wheel_bet (state, data) { state.socket.subscribes.wheel_bet = data },
+
+    SOCKET_roulette (state, data) { state.socket.subscribes.roulette = data },
+    SOCKET_roulette_bet (state, data) { state.socket.subscribes.roulette_bet = data },
 
     SOCKET_disconnect (state, reload) {
       clearTimeout(state.socket.socketTimeout)
@@ -192,7 +239,7 @@ const moduleSocket = {
       state.socket.isAuthSocket = false
       state.socket.isConnected = true
 
-      state.socket.isReload = false
+      //state.socket.isReload = false
       state.socket.isClosed = false
 
       clearTimeout(state.socket.socketTimeout)
@@ -204,7 +251,11 @@ const moduleSocket = {
       }, 35000)
     },
     SOCKET_ONCLOSE (state, event) {
-      if (state.socket.isReload || state.socket.isClosed) return
+      if (state.socket.isClosed) return
+      if (state.socket.isReload) {
+        state.socket.isReload = false
+        return
+      }
 
       console.log('socket closed ', event)
       clearTimeout(state.socket.socketTimeout)
